0364271fc109abd19c37d62c7b6d248d276470d5
kayzee <olaniyikazeem112@gmail.com>
2022-08-17 19:50:50 +0100

    updated

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -36,20 +36,26 @@ contract Swap{
 
     }
 
+    
+
     function SwapExactTokentoToken (address tokenaddr, address tokenaddr2,address owner1, address owner2,uint tokenvalue1, uint tokenvalue2) public {
-        for(uint256 i=0; i < pricefeed.length; i++){
-            if(pricefeed[i].tokenAddress == tokenaddr){
-                uint checkprice1 = pricefeed[i].price;
-            }
-            if(pricefeed[i].tokenAddress == tokenaddr2){
-                uint checkprice2 = pricefeed[i].price;
-            }
-        }
         IERC20 exactToken = IERC20(tokenaddr);
         IERC20 token = IERC20(tokenaddr2);
 
         require(exactToken.allowance(owner1, address(this)) >= tokenvalue1, "You are withdrawing more than your allowance");
         require(token.allowance(owner2, address(this)) >= tokenvalue2, "You are withdrawing more than your allowance");
 
-        exactToken.transferFrom(owner1, owner2, tokenvalue1);
-        token.transferFrom(owner2, owner1, (tokenvalue2 * 1/2));
+        for(uint256 i=0; i < pricefeed.length; i++){
+            if(pricefeed[i].tokenAddress == tokenaddr){
+                uint256 checkprice1 = pricefeed[i].price;
+                token.transferFrom(owner2, owner1, (tokenvalue2 * checkprice1));
+            }
+            if(pricefeed[i].tokenAddress == tokenaddr2){
+                uint256 checkprice2 = pricefeed[i].price;
+                exactToken.transferFrom(owner1, owner2, (tokenvalue1 * checkprice2));
+            }
+        }
+
+        
+        
+    }
